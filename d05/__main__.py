ad7baf5ab833ccfae615f6ff34be07286cac58ab
Konstantin Spirin <konstmsu@gmail.com>
2023-12-06 02:25:45 +0800

    day 5 part 2

--- d05/__main__.py
+++ d05/__main__.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-                                       
+
 lines = (Path(__file__).parent / "input.txt").read_text().splitlines(keepends=False)
 maps = []
 
@@ -12,17 +12,21 @@ for line in lines[1:]:
     else:
         maps[-1].append([int(v) for v in line.split(" ")])
 
+
 def get_next(layer: list[int], value: int):
     for t, f, n in layer:
         if f <= value < f + n:
-            return t + value - f
+            return t + value - f, f + n - value
+
 
 def trace(start: int):
     value = start
-    for  layer in (maps):
+    for layer in maps:
         nv = get_next(layer, value)
-        value = value if nv is None else nv
+        if nv:
+            value = nv[0]
     return value
+
 
 # print(f"{seeds=}")
 # print(f"{maps=}")
@@ -30,3 +34,28 @@ best = min(seeds, key=trace)
 print(f"{best=}, {trace(best)=}")
 
 # 1 part answer 289863851
+
+
+def trace2(start: int):
+    value = start
+    min_ff = 1_000_000_000_000
+    for layer in maps:
+        nv = get_next(layer, value)
+        if nv:
+            value = nv[0]
+            min_ff = min(min_ff, nv[1])
+    return value, min_ff
+
+
+min_loc = 1_000_000_000_000
+for i in range(len(seeds))[::2]:
+    f, t = seeds[i : i + 2]
+    j = 0
+    while j < t:
+        v = f + j
+        loc, ff = trace2(v)
+        min_loc = min(min_loc, loc)
+        j += ff
+
+print(f"{min_loc=}")
+# part 2 answer is min_loc=60568880
