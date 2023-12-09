b75912e88ceb189c58e809f6e817f0dc0ca1bd43
Konstantin Spirin <konstmsu@gmail.com>
2023-12-09 20:48:42 +0800

    day 8 part 2

--- d08/__main__.py
+++ d08/__main__.py
@@ -1,6 +1,7 @@
 from functools import cache
 from pathlib import Path
 import re
+from math import lcm
 
 lines = (Path(__file__).parent / "input.txt").read_text().splitlines(keepends=False)
 
@@ -26,6 +27,9 @@ def part1():
 def part2():
     @cache
     def advance(start: str, pointer: int, count: int) -> str:
+        if not (0 <= pointer < len(dirs)):
+            raise ValueError()
+
         s = start
         for i in range(count):
             d = dirs[(pointer + i) % len(dirs)]
@@ -45,20 +49,49 @@ def part2():
             if pos[-1] == "Z":
                 return count
 
+    def find_period(start: str):
+        v = start
+        first_z = None
+        period = []
+        pointer = 0
+        while True:
+            d = dist_to_z(v, pointer)
+            period.append(d)
+            v = advance(v, pointer, d)
+            pointer = (pointer + d) % len(dirs)
+            if first_z is None:
+                first_z = v
+            else:
+                if first_z == v:
+                    break
+        return period
+
     ghosts = [k for k in mapping.keys() if k[-1] == "A"]
-    d2 = 0
-    pointer = 0
-    counter = 0
-    while any(g[-1] != "Z" for g in ghosts):
-        d = max(dist_to_z(g, pointer) for g in ghosts)
-        d2 += d
-        counter += 1
-        if counter % 100000 == 0:
-            print(d2)
-        ghosts = [advance(g, pointer, d) for g in ghosts]
-        pointer = (pointer + d) % len(dirs)
+    periods = [find_period(g) for g in ghosts]
+    result = lcm(*[v[1] for v in periods])
+    print(f"{result=}")
+
+    print(periods)
+    print(result)
+    for g, p in zip(ghosts, periods):
+        a = result % len(dirs)
+        r = advance(g, 0, p[0])
+        print(f"{a=}, {r=}")
+    # d2 = 0
+    # pointer = 0
+    # counter = 0
+    # while any(g[-1] != "Z" for g in ghosts):
+    #     d = max(dist_to_z(g, pointer) for g in ghosts)
+    #     d2 += d
+    #     counter += 1
+    #     if counter % 100000 == 0:
+    #         print(d2)
+    #     ghosts = [advance(g, pointer, d) for g in ghosts]
+    #     pointer = (pointer + d) % len(dirs)
 
 
 part2()
 
 # 12926477700000 is too low
+# 25855201539218 is too high
+# 12927600769609 is correct
