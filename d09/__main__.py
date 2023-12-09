4fff2ccd354b719aa566c059c724134af0d52d26
Konstantin Spirin <konstmsu@gmail.com>
2023-12-09 21:29:19 +0800

    day 9 part 2

--- d09/__main__.py
+++ d09/__main__.py
@@ -25,4 +25,29 @@ def part1():
     print(f"{result=}")
 
 
-part1()
+# 1731106378 is correct
+
+
+def part2():
+    def process(line: list[int]):
+        print(f"Starting {line}")
+
+        vv = [list(line)]
+        while any(v != 0 for v in vv[-1]):
+            vv.append([b - a for a, b in zip(vv[-1], vv[-1][1:])])
+
+        for i in reversed(range(len(vv) - 1)):
+            vv[i].insert(0, vv[i][0] - vv[i + 1][0])
+
+        for line in vv:
+            print(line)
+
+        return vv
+
+    result = sum(process(nn)[0][0] for nn in nums)
+    print(f"{result=}")
+
+
+part2()
+
+# 1087 is correct
